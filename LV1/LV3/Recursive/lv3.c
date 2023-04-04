95a46ca7656131da19775cbbca10bdec9b87165c
Igor <107479596+Iggy2399@users.noreply.github.com>
2023-04-04 23:17:08 +0200

    Add files via upload

---
+++ LV1/LV3/Recursive/lv3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int povrh(int m, int n);
+int main(){
+    int n=10;
+    int m = n/2;
+    /*printf("Unesite n:");
+    scanf("%d", &n);*/
+    printf("%d",povrh(n,m));
+
+}
+
+int povrh(int n, int m){
+    if(m == 0 || m == n){
+        return 1;
+    }
+    else{
+        return povrh(n-1, m-1) + povrh(n-1, m);
+    }
+    
+    
+}
